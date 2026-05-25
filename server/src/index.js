5e6daad522141e1beb447fc8c630ad28300b09f5
Yogesh <ykm9586@gmail.com>
2026-05-25 23:11:19 +0530

    Fix production cors

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.CLIENT_URL,
+        origin: "*",
         methods: ["GET", "POST"],
     },
 });
@@ -27,7 +27,11 @@ const io = new Server(server, {
 global.io = io;
 
 app.set("io", io);
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(express.json());
 
 app.use("/api/leads", leadRoutes);
